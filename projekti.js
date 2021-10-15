999fc4d71842ccd5d1375d07da9077c335a42c5b
nmnn33 <tommy123tran@gmail.com>
2021-10-15 10:44:27 +0300

    ka

--- projekti.js
+++ projekti.js
@@ -1,1 +1,7 @@
-console.log("homo");
+function Itsesyöte() {
+    var teksti = document.getElementById("Tapahtuma");
+    var piilo = document.getElementById("Itsesyöte")
+    if (teksti.value == "Itse") {
+        piilo.style.display = "block";
+    }
+}
