e829571f5f558f4839795427bfc4d96ad11890da
nmnn33 <tommy123tran@gmail.com>
2021-10-15 11:19:32 +0300

    jj

--- projekti.js
+++ projekti.js
@@ -1,7 +1,14 @@
 function Itsesyöte() {
     var teksti = document.getElementById("Tapahtuma");
-    var piilo = document.getElementById("Itsesyöte")
+    var piilo = document.getElementById("Itses")
     if (teksti.value == "Itse") {
         piilo.style.display = "block";
+        document.getElementById("itses").required = true;
+    } else {
+        piilo.style.display = "none";
     }
+}
+
+function lisääYksi() {
+
 }
